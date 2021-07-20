54342739ef2148ecc3f14ef19535e4c258966261
Alessandro <alessanbiz@gmail.com>
2021-07-19 22:29:26 -0300

    feat: Transaction card creation and component typing

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,9 @@
+import { FlatList } from 'react-native';
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { DataListProps } from '.';
 
 export const Container = styled.View`
   flex: 1;
@@ -64,3 +66,25 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  margin-bottom: ${RFValue(16)}px;
+;`;
+
+export const TransactionsList = styled(
+  FlatList as new () => FlatList<DataListProps>).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {
+      paddingBottom: getBottomSpace()
+    }
+  })``;
